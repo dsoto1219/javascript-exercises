68b1a32783970bdd7794b1882a2953e71853f570
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-02 14:24:53 -0500

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstTerm, lastTerm) {
+    // Checks
+    if (firstTerm > lastTerm) [firstTerm, lastTerm] = [lastTerm, firstTerm];
+    for (const arg of arguments) {
+        if (arg < 0 || !Number.isInteger(arg)) {
+            return 'ERROR';
+        }
+    }
 
+    // Algorithm
+    let sum = 0;
+    for (let i = firstTerm; i <= lastTerm; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
