cc334a07a22f58f84fb431a39b93893aa4313648
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-02 14:30:51 -0500

    complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // If year divisible by 400, good
+    if (year % 400 == 0) {
+        return true;
+    /*
+     Otherwise, because divisibility by 400
+     implies divisibility by either 100 or 4,
+     check if not divisible by 100 and divisible by
+     4.
+    */
+    } else if (year % 100 != 0 && year % 4 == 0) {
+        return true;
+    // Otherwise, false (may be divisible by 100, but we don't care)
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
