2e6e0f8dafc3f23a54d087ccd95a27a94804a17c
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-02 13:50:38 -0500

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    for (const element of elements) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === element) {
+                array.splice(i, 1); // operation shifts all indices down by one
+                i--; // shift index down by one to compensate
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
