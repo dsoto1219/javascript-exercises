b66cb41a0fdf0399305b3b928a31bd91eda77d09
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-09 16:27:56 -0400

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    // Remove all non-word characters, make lowercase
+    const cleanString = string.replace(/\W/g, '').toLowerCase(); 
+    const cleanStringRev = cleanString.split('').reverse().join('');
+    return cleanString === cleanStringRev;
 };
 
 // Do not edit below this line
