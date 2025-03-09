8e8aa82393903ac1f41b2d4f282821eb04d82ffb
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-09 16:37:03 -0400

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let N = parseInt(n);
+    if (N < 0) return "OOPS";
 
+    let arr = [0, 1];
+    for (let i = 1; i < N; i++) {
+        let next = arr[i] + arr[i-1];
+        arr.push(next);
+    }
+    return arr[n];
 };
 
 // Do not edit below this line
