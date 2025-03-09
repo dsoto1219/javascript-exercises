ac77d897a1854841f09275760982ac6aee6f58f2
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2025-03-09 16:48:09 -0400

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a,b) => {
+        const thisYear = 2025;
+        let aAge;
+        let bAge;
+        if ("yearOfDeath" in a) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            aAge = thisYear - a.yearOfBirth;
+        }
+        if ("yearOfDeath" in b) {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            bAge = thisYear - b.yearOfBirth;
+        }
+        return aAge - bAge;
+    })    
+    return people.at(-1);
 };
 
 // Do not edit below this line
